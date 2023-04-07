4964f208d3ef2263b45d770356f3ac838290452c
David Peguero <david.peguero.cancel@gmail.com>
2023-04-07 19:08:07 -0400

    Added icons and some styling to the forecasts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?appid=3be2b2b6acc21e3760901d15acf91f72&q=Dallas&cnt=6&units=imperial'
 var forecastListEl = document.getElementById('forecast-list')
+
+var forcastLiClasses = "day d-inline border border-black bg-dark text-light".split(" ");
 function getWeatherData(){
     fetch(requestUrl).
         then(function(response){
@@ -7,6 +9,8 @@ function getWeatherData(){
                 return response.json();
             }
         }).then(function (data){
+            
+
 
             //For loop that goes over the next 5 days 
             for(var i = 1; i < data.list.length; i++){
@@ -18,18 +22,24 @@ function getWeatherData(){
                 //Create Li and corresponding elements
                 var forecastDayLi = document.createElement('li');
                 var date = document.createElement('p');
+                var weatherImg = document.createElement('img');
                 var temp = document.createElement('p');
                 var wind = document.createElement('p');
                 var humidity = document.createElement('p');
 
                 //Assign the values of each element in li
-                date.innerText = dayjs.unix(forecastDay.dt).format('M/DD/YYYY')
-                temp.innerText = forecastDay.temp.day;
-                wind.innerText = forecastDay.speed;
-                humidity = forecastDay.humidity;
+                date.innerText = dayjs.unix(forecastDay.dt).format('M/DD/YYYY');
+                weatherImg.src = `https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png`
+                temp.innerText = 'Temp: ' + forecastDay.temp.day + '°F';
+                wind.innerText = 'Wind: ' + forecastDay.speed + ' MPH';
+                humidity = 'Humidity: ' + forecastDay.humidity + '%';
 
+                //Classes for the li and inner attributes
+                forecastDayLi.classList.add('d-inline', 'border' ,'border-black' ,'bg-dark' ,'text-light', 'p-3');
+                date.classList.add('fw-bold');
                 //Append each attribute of the forecast to the forecast li
-                forecastDayLi.append(date)
+                forecastDayLi.append(date);
+                forecastDayLi.append(weatherImg);
                 forecastDayLi.append(temp);
                 forecastDayLi.append(wind);
                 forecastDayLi.append(humidity);
@@ -40,6 +50,6 @@ function getWeatherData(){
         });
 }
 
-getWeatherData();
+// getWeatherData();
 
 
