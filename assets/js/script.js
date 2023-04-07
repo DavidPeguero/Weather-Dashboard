d6d12770fae2adfff0446a53712a8dfbd40495d8
David Peguero <david.peguero.cancel@gmail.com>
2023-04-07 18:41:52 -0400

    Created for loop to create forecast li elements and append them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily?appid=3be2b2b6acc21e3760901d15acf91f72&q=Dallas&cnt=6&units=imperial'
-
+var forecastListEl = document.getElementById('forecast-list')
 function getWeatherData(){
     fetch(requestUrl).
         then(function(response){
@@ -9,6 +9,23 @@ function getWeatherData(){
         }).then(function (data){
             for(var i = 1; i < data.list.length; i++){
                 console.log(data.list[i]);
+                var forecastDay = data.list[i]; 
+                var forecastDayLi = document.createElement('li');
+                var date = document.createElement('p');
+                var temp = document.createElement('p');
+                var wind = document.createElement('p');
+                var humidity = document.createElement('p');
+                date.innerText = dayjs.unix(forecastDay.dt).format('M/DD/YYYY')
+                temp.innerText = forecastDay.temp.day;
+                wind.innerText = forecastDay.speed;
+                humidity = forecastDay.humidity;
+                forecastDayLi.append(date)
+                forecastDayLi.append(temp);
+                forecastDayLi.append(wind);
+                forecastDayLi.append(humidity);
+
+                forecastListEl.append(forecastDayLi);
+
             }
         });
 }
